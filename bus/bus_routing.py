b1323e69c049fff611d127d650a20409a60cc623
REDACTED <REDACTED_EMAIL>
2023-09-27 21:31:18 -0400

    bus codes

---
+++ bus/bus_routing.py
@@ -0,0 +1,167 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+import json
+
+from qiskit.utils import algorithm_globals
+from qiskit.algorithms.minimum_eigensolvers import SamplingVQE
+from qiskit.algorithms.optimizers import SPSA
+from qiskit.circuit.library import RealAmplitudes
+from qiskit.primitives import Sampler
+
+from qiskit_optimization import QuadraticProgram
+from qiskit_optimization.algorithms import MinimumEigenOptimizer
+
+# Received from the Qiskit Vehicle Routing tutorial: https://qiskit.org/ecosystem/optimization/tutorials/07_examples_vehicle_routing.html
+class QuantumOptimizer:
+    def __init__(self, instance, n, K):
+
+        self.instance = instance
+        self.n = n
+        self.K = K
+
+    def binary_representation(self, x_sol=0):
+
+        instance = self.instance
+        n = self.n
+        K = self.K
+
+        A = np.max(instance) * 100  # A parameter of cost function
+
+        # Determine the weights w
+        instance_vec = instance.reshape(n**2)
+        w_list = [instance_vec[x] for x in range(n**2) if instance_vec[x] > 0]
+        w = np.zeros(n * (n - 1))
+        for ii in range(len(w_list)):
+            w[ii] = w_list[ii]
+
+        # Some variables I will use
+        Id_n = np.eye(n)
+        Im_n_1 = np.ones([n - 1, n - 1])
+        Iv_n_1 = np.ones(n)
+        Iv_n_1[0] = 0
+        Iv_n = np.ones(n - 1)
+        neg_Iv_n_1 = np.ones(n) - Iv_n_1
+
+        v = np.zeros([n, n * (n - 1)])
+        for ii in range(n):
+            count = ii - 1
+            for jj in range(n * (n - 1)):
+
+                if jj // (n - 1) == ii:
+                    count = ii
+
+                if jj // (n - 1) != ii and jj % (n - 1) == count:
+                    v[ii][jj] = 1.0
+
+        vn = np.sum(v[1:], axis=0)
+
+        # Q defines the interactions between variables
+        Q = A * (np.kron(Id_n, Im_n_1) + np.dot(v.T, v))
+
+        # g defines the contribution from the individual variables
+        g = (
+            w
+            - 2 * A * (np.kron(Iv_n_1, Iv_n) + vn.T)
+            - 2 * A * K * (np.kron(neg_Iv_n_1, Iv_n) + v[0].T)
+        )
+
+        # c is the constant offset
+        c = 2 * A * (n - 1) + 2 * A * (K**2)
+
+        try:
+            max(x_sol)
+            # Evaluates the cost distance from a binary representation of a path
+            fun = (
+                lambda x: np.dot(np.around(x), np.dot(Q, np.around(x)))
+                + np.dot(g, np.around(x))
+                + c
+            )
+            cost = fun(x_sol)
+        except:
+            cost = 0
+
+        return Q, g, c, cost
+
+    def construct_problem(self, Q, g, c, n) -> QuadraticProgram:
+        qp = QuadraticProgram()
+        for i in range(n * (n - 1)):
+            qp.binary_var(str(i))
+        qp.objective.quadratic = Q
+        qp.objective.linear = g
+        qp.objective.constant = c
+        return qp
+
+    def solve_problem(self, qp):
+        algorithm_globals.random_seed = 10598
+        vqe = SamplingVQE(sampler=Sampler(), optimizer=SPSA(), ansatz=RealAmplitudes())
+        optimizer = MinimumEigenOptimizer(min_eigen_solver=vqe)
+        result = optimizer.solve(qp)
+        # compute cost of the obtained result
+        _, _, _, level = self.binary_representation(x_sol=result.x)
+        return result.x, level
+    
+# Visualize the solution
+def visualize_solution(xc, yc, x, C, n, K, title_str):
+    # Put the solution in a way that is compatible with the classical variables
+    x_quantum = np.zeros(n**2)
+    kk = 0
+    for ii in range(n**2):
+        if ii // n != ii % n:
+            x_quantum[ii] = x[kk]
+            kk += 1
+
+    plt.figure()
+    plt.scatter(xc, yc, s=200)
+    for i in range(len(xc)):
+        plt.annotate(i, (xc[i] + 0.15, yc[i]), size=16, color="r")
+    plt.plot(xc[0], yc[0], "r*", ms=20)
+
+    plt.grid()
+
+    for ii in range(0, n**2):
+
+        if x_quantum[ii] > 0:
+            ix = ii // n
+            iy = ii % n
+            plt.arrow(
+                xc[ix],
+                yc[ix],
+                xc[iy] - xc[ix],
+                yc[iy] - yc[ix],
+                length_includes_head=True,
+                head_width=0.25,
+            )
+
+    plt.title(title_str + " cost = " + str(int(C * 100) / 100.0))
+    plt.show()
+
+class BusRoutingInstance:
+    def __init__(self, n):
+        self.n = n
+
+    def generate_instance(self, depot_locations):
+
+        n = self.n
+
+        # np.random.seed(33)
+        np.random.seed(1543)
+
+        # Generate the coordinates of the depots
+        xc = np.zeros(n)
+        yc = np.zeros(n)
+
+        for ii in range(0, n):
+            xc[ii] = depot_locations[ii]["lat"]
+            yc[ii] = depot_locations[ii]["lng"]
+
+        instance = np.zeros([n, n])
+        for ii in range(0, n):
+            for jj in range(ii + 1, n):
+                instance[ii, jj] = (xc[ii] - xc[jj]) ** 2 + (yc[ii] - yc[jj]) ** 2 # Euclidean distance
+                instance[jj, ii] = instance[ii, jj]
+
+        return xc, yc, instance
+
+
+
