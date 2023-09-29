dc8b6393a27298fc61da261e8d3ce91ca80c0ba5
REDACTED <REDACTED_EMAIL>
2023-09-29 02:34:45 -0400

    complete g function, modularized code

---
+++ bus/placement/getter_functions.py
@@ -0,0 +1,113 @@
+import random
+import csv
+
+def get_bus_station_location(file_path):
+    """
+    Extracts the latitude, longitude and name of the bus stations from the csv file
+    """
+    # Initialize empty lists to store the extracted values
+    latitudes = []
+    longitudes = []
+    names = []
+
+    with open(file_path, 'r') as csv_file:
+        reader = csv.reader(csv_file)
+        next(reader)  # Skip the header row if present
+
+        # Loop over each row in the csv using reader object
+        for row in reader:
+            # row variable is a list that represents a row in csv
+            latitude = float(row[1])
+            longitude = float(row[2])
+            name = row[0]
+            # Append the values to the respective lists
+            latitudes.append(latitude)
+            longitudes.append(longitude)
+            names.append(name)
+
+    coordinates = {}
+    for lat, lon, n in zip(latitudes, longitudes, names):
+        coordinates[n] = [lat,lon]
+    
+    return coordinates
+
+
+def select_bus_station(coordinates: dict, start=1, end=20, lat=40.769033, lon=-73.969649):
+    """
+    Selects bus stations from provided stations within the specified distance
+
+    Parameters
+    ----------
+    coordinates : dict
+        Dictionary containing the latitude and longitude of the bus stations
+    start : int
+        Starting distance from the Central Park with latitude and longitude of 40.769033, -73.969649. (default)
+    end : int
+        Ending distance from the Central Park with latitude and longitude of 40.769033, -73.969649. (default)
+
+    Returns
+    -------
+    selected_coordinates : dict
+        Dictionary containing the latitude and longitude of the selected bus stations
+    """
+    selected_coordinates = {}
+    for k,v in coordinates.items():
+        if (v[0]-lat)**2 + (v[1]+lon)**2 >= start/100 and (v[0]-lat)**2 + (v[1]+lon)**2 <= end/100:
+            selected_coordinates[k] = v
+    
+    return selected_coordinates
+
+def get_station_distance(file):
+    """
+    Extracts the distance of the bus stations from the csv file
+    """
+    stations_dic = {}
+
+    with open(file, "r") as input_file:
+        next(input_file, None)
+        for line in input_file:
+            fields = line.strip().split(",")
+            start = fields[0]
+            end = fields[1]
+            duration = float(fields[2])
+            key = (start, end)
+            stations_dic[key] = duration
+
+    return stations_dic
+
+# calculate the f and g for each station
+# f is the number of people getting off at the station
+# g is the distance from the nearest touristic site
+
+def get_no_of_people_at_station(selected_coordinates):
+    pop_at_station = {}
+    with open("./data/station_pop_clean.csv", "r") as input_file:
+        next(input_file, None)
+        for line in input_file:
+            fields = line.strip().split(",")
+            station = fields[1]
+            pop = float(fields[4])
+            if station in selected_coordinates.keys():
+                pop_at_station[station] = pop
+    
+    return pop_at_station
+
+def get_distance_from_nearest_site(selected_coordinates):
+    distance_from_site = {}
+    with open("./data/bus_metro_distance.csv", "r") as input_file:
+        next(input_file, None)
+        for line in input_file:
+            fields = line.strip().split(",")
+            station = fields[0]
+            distance = float(fields[2])
+            if station in selected_coordinates.keys():
+                distance_from_site[station] = distance
+    
+    return distance_from_site
+
+def normalize(dic):
+    max_value = max(dic.values())
+    min_value = min(dic.values())
+    for key in dic.keys():
+        dic[key] = (dic[key] - min_value) / (max_value - min_value)
+    return dic
---
+++ bus/placement/graph_utils.py
@@ -0,0 +1,100 @@
+from getter_functions import get_no_of_people_at_station, get_distance_from_nearest_site, normalize
+
+import rustworkx
+import matplotlib.pyplot as plt
+from rustworkx.visualization import mpl_draw
+
+def make_node_edge(selected_coordinates, stations_dic):
+    """
+    Make a list of nodes and edges from the selected coordinates and stations dictionary
+    
+    Parameters
+    ----------
+    selected_coordinates : dict
+        Dictionary of selected coordinates
+    stations_dic : dict
+        Dictionary of stations and their distances
+
+    Returns
+    -------
+    node_dic : dict
+        Dictionary of nodes
+    index_dic : dict
+        Dictionary of node indices
+    edge_dic : dict
+        Dictionary of edges
+    """
+    #node dictionary
+    node_dic = {}
+    f_list = get_no_of_people_at_station(selected_coordinates)
+    g_list = get_distance_from_nearest_site(selected_coordinates)
+
+    f_list = normalize(f_list)
+    g_list = normalize(g_list)
+
+    # print(f_list)
+
+    for (key, _) in selected_coordinates.items():
+        # print(key)
+        if (key in f_list) and (key in g_list):
+            node_dic[key] = { 'name':key , 'f': f_list[key] , 'g': g_list[key]}
+
+    C = 2
+    D = 2
+
+    temp = {node: {"name": attrs["name"], "c": C*attrs["f"] + D*attrs["g"]}
+                    for node, attrs in node_dic.items()}
+    node_dic = temp
+
+    #node index dictionary
+    index_dic = {}
+
+    index = 0
+    for node in node_dic:
+        index_dic[node] = index
+    index+=1
+
+    # #edge dictionary
+    # edge_dic = {}
+
+    # # edges only between nodes w 30 min distance
+    # for item in stations_dic.items():
+    #   dur = item[1]
+    #   if dur <= 30:
+    #     edge_dic[item[0]] = {'cost': dur}
+
+    # will add edges for all the nodes considered
+    edge_dic = {}
+    for item in stations_dic.items():
+        if (item[0][0] in node_dic) and (item[0][1] in node_dic) and item[1] <= 25:
+            edge_dic[item[0]] = {'cost': item[1]}
+
+    return node_dic, index_dic, edge_dic
+
+def make_graph(node_dic, index_dic, edge_dic):
+    """
+    Make a graph from the node and edge dictionaries
+    
+    Parameters
+    ----------
+    node_dic : dict
+        Dictionary of nodes
+    index_dic : dict
+        Dictionary of node indices
+    edge_dic : dict
+        Dictionary of edges
+
+    Returns
+    -------
+    graph : rustworkx
+        Graph of nodes and edges
+    """
+    graph = rustworkx.Graph()
+
+    for node in node_dic:
+        graph.add_node(index_dic[node], **node_dic[node])
+
+    for edge in edge_dic:
+        graph.add_edge(index_dic[edge[0]], index_dic[edge[1]], **edge_dic[edge])
+
+    return graph
