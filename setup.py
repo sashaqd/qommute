86037cb90b697feceffde34fd122dfc7b903597d
Sasha-Malik <106297572+Sasha-Malik@users.noreply.github.com>
2023-09-30 12:00:54 +0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='Qommute',
+    version='0.0.1',
+    description='Optimization bus station placements and routing using quantum computing and machine learning model.',
+    author= 'Samyam Lamichhane Sarthak Malla Sasha',
+    url = 'https://github.com/Sasha-Malik/Qommute',
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    keywords=['route optimization', 'quantum computing', 'machine learning', 'bus station placement'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    py_modules=['Qommute'],
+    package_dir={'':'src'},
+    install_requires = [
+        'pytorch',
+        'qiskit',
+        'rustworkx',
+        'pyqubo', 
+        'sklearn'
+    ]
+)
